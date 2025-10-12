e391e58a481f25e86b04646642336e1a468a5d04
kamal <kamalnet@gmail.com>
2025-10-12 01:20:00 +0000

    Conflict Resoved Sales Module Three

--- sales.java
+++ sales.java
@@ -3,3 +3,12 @@ public class Main {
     System.out.println("Hello World!");
   }
 }
+// Main method
+public static void main(String[] args) {
+myStaticMethod(); // Call the static method
+// myPublicMethod(); This would compile an error
+    Main myObj = new Main(); // Create an object of Main
+    myObj.myPublicMethod(); // Call the public method on the object
+  }
+}
+(Sales Module Three)
