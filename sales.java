16fc67e2045918deedd5596bca30cfa250a665f1
kamal <kamalnet@gmail.com>
2025-10-12 01:48:17 +0000

    Sales Four

--- sales.java
+++ sales.java
@@ -18,3 +18,8 @@ myStaticMethod(); // Call the static method
     myCar.speed(200);          // Call the speed() method
   }
 }
+public static void main(String[] args) {
+    Main myObj = new Main(); // Create an object of class Main (This will call the constructor)
+    System.out.println(myObj.x); // Print the value of x
+  }
+}
